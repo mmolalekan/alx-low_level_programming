2ebdaf3d0f6f539828b3eb3a818cbdf651763689
mmolalekan <muhayminolakena@yahoo.com>
2023-02-21 00:56:48 +0100

    mm

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,13 @@
 * main - Prints the alphabet.
 *
 * Return: Always 0 (Success)
-*/
+      */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
