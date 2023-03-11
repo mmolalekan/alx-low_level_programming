eb154bfd13de71776e229ef3bad51fc0fdc0cf28
mmolalekan <muhayminolalekan@yahoo.com>
2023-03-11 13:03:42 +0100

    task 5 trial 3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,10 @@
 /**
 * print_sign - prints the sign of a number.
 *@n: argument to check
-*Return: 1 and prints + if n is greater than zero
-* 0 and prints 0 if n is zero
-* -1 and prints - if n is less than zero
+*Return: 1 and prints + when n is greater than zero
+*0 and prints 0 if n is zero
+*-1 and prints - if n is less than zero
+*/
 
 int print_sign(int n)
 {
