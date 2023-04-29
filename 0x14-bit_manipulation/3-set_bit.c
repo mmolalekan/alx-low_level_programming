97786072b37ef58793f0db567da0989afe18fdb9
mmolalekan <muhayminolalekan@yahoo.com>
2023-04-29 01:33:40 +0100

    101-password

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit -  sets the value of a bit to 1 at a given index.
  * @n: pointer to the number to be changed
  * @index: index of the bit to set to 1
- * Return: 1 if it works, -1 if an error occured 
+ * Return: 1 if it works, -1 if an error occured
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
